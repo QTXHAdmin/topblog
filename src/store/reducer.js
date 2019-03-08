8b545a8eddf57732ba4372992ac1f92c474cce0c
zhanghe888 <15942892352@sina.cn>
2019-03-08 20:17:12 +0800

    prepare to Extract form components

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -4,5 +4,6 @@ import { combineReducers } from 'redux-immutable';
 import { reducer as loginReducer } from '../pages/login/store';
 const reducer = combineReducers({
   login: loginReducer
+
 });
 export default reducer;
