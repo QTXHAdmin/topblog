3a1e162d792af4dbbd514c60dcd400ed47010fc8
zhanghe888 <15942892352@sina.cn>
2019-03-10 16:12:09 +0800

    update one more time

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Login from './pages/login';
 import Home from './pages/home';
 import Myblog from './pages/system/myblog';
@@ -6,10 +6,15 @@ import Nomatch from './pages/nomatch';
 import Manageblog from './pages/system/manageblog';
 import Writeblog from './pages/system/writeblog';
 import Markedownedit from './pages/system/markedownedit';
-import {HashRouter as Router, Route, Switch, Redirect} from 'react-router-dom';
+import {
+  HashRouter as Router,
+  Route,
+  Switch,
+  Redirect
+} from 'react-router-dom';
 class App extends Component {
-  checkUserState(){
-    if(sessionStorage.getItem('APP_LOGIN_USER')){
+  checkUserState() {
+    if (sessionStorage.getItem('APP_LOGIN_USER')) {
       return true;
     }
     return false;
@@ -18,21 +23,27 @@ class App extends Component {
     return (
       <Router>
         <Switch>
-          <Route path="/" exact render={ () => <Redirect to="/home"></Redirect> }></Route>
-          <Route path="/login" exact component={Login}/>
+          <Route path="/" exact render={() => <Redirect to="/home" />} />
+          <Route path="/login" exact component={Login} />
           <Route path="/home" component={Home} />
-          <Route path="/home" render={(props)=>{
-            if(this.checkUserState()){
-              return <Home {...props}/>
-            }
-            sessionStorage.setItem('APP_LAST_URL',JSON.stringify(props.location))
-            return <Redirect to="/login" />
-          }}/>
-          <Route path="/myblog" component={Myblog}/>
-          <Route path="/manageblog" component={Manageblog}/>
-          <Route path="/writeblog" component={Writeblog}/>
-          <Route path="/markdownedit" component={Markedownedit}/>
-          <Route component={Nomatch}/>
+          <Route
+            path="/home"
+            render={props => {
+              if (this.checkUserState()) {
+                return <Home {...props} />;
+              }
+              sessionStorage.setItem(
+                'APP_LAST_URL',
+                JSON.stringify(props.location)
+              );
+              return <Redirect to="/login" />;
+            }}
+          />
+          <Route path="/myblog" component={Myblog} />
+          <Route path="/manageblog" component={Manageblog} />
+          <Route path="/writeblog" component={Writeblog} />
+          <Route path="/markdownedit" component={Markedownedit} />
+          <Route component={Nomatch} />
         </Switch>
       </Router>
     );
