7b14d145ee2ee9857d8931418d63c3625a2c605f
zhao5840980 <2439421565@qq.com>
2019-03-11 14:58:50 +0800

    tijiaogenxin

--- src/pages/system/myblog/index.js
+++ src/pages/system/myblog/index.js
@@ -3,7 +3,8 @@ import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 import Header from '../../../components/Header';
 import './style.less';
-import { Checkbox, Button, Avatar } from 'antd';
+import { Checkbox, Button } from 'antd';
+import BlogLeft from '../../../components/blogleftmsg';
 class Myblog extends Component {
   render() {
     const { loginStatus } = this.props;
@@ -24,55 +25,7 @@ class Myblog extends Component {
             </div>
           </div>
           <div className="myBlogBody">
-            <div className="myBlogBody-l">
-              <div className="l-top">
-                <Avatar size="large" icon="user" />
-                <span className="username">zhengzheng</span>
-              </div>
-              <div className="l-middle">
-                <ul className="l-middlelist">
-                  <li>
-                    <p>原创</p>
-                    <p>0</p>
-                  </li>
-                  <li>
-                    <p>粉丝</p>
-                    <p>0</p>
-                  </li>
-                  <li>
-                    <p>喜欢</p>
-                    <p>0</p>
-                  </li>
-                  <li>
-                    <p>评论</p>
-                    <p>0</p>
-                  </li>
-                </ul>
-              </div>
-              <div className="l-bottom">
-                <p>
-                  <span>等级：</span>
-                  <span>博客1</span>
-                </p>
-                <p>
-                  <span>访问: </span>
-                  <span>3</span>
-                </p>
-                <p>
-                  <span>积分: </span>
-                  <span>10</span>
-                </p>
-                <p>
-                  <span>排名: </span>
-                  <span>269万+</span>
-                </p>
-              </div>
-              <div className="l-hot">
-                <span className="l-hottext">热门文章</span>
-                <div></div>
-              </div>
-
-            </div>
+            <BlogLeft />
             <div className="myBlogBody-r">
               <div className="r-title">
                 <Checkbox>只看原创</Checkbox>
